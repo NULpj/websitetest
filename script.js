d3b4b53ef79c241c79e78f858520fc94c8b40126
idk <random.exe.nul@gmail.com>
2025-01-04 15:03:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// JavaScript to change the message on button click
+document.getElementById('clickButton').addEventListener('click', function() {
+    document.getElementById('message').innerHTML = 'You clicked the button! 🎉';
+});
